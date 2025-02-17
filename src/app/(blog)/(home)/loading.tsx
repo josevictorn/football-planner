761aa83ae1b7c644a30ff35f1784c82d684e215a
José Victor <josevictornascimento2016@gmail.com>
2025-02-16 22:01:52 -0300

    feat: created loading pages

--- src/app/(blog)/(home)/loading.tsx
+++ src/app/(blog)/(home)/loading.tsx
@@ -1,3 +1,32 @@
+import { Skeleton } from "@/components/skeleton";
+
 export default function PostLoading() {
-  return <p>carregando...</p>;
+  return (
+    <div className="flex gap-12 flex-col">
+      <Skeleton className="h-9 w-60" />
+
+      <Skeleton className="h-64 w-full" />
+
+      <main className="flex flex-col gap-8">
+        <div className="flex flex-col gap-3">
+          <Skeleton className="h-6 w-32" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <div className="flex flex-col gap-3">
+          <Skeleton className="h-6 w-32" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <div className="flex flex-col gap-3">
+          <Skeleton className="h-6 w-32" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/app/(blog)/[slug]/loading.tsx
+++ src/app/(blog)/[slug]/loading.tsx
@@ -1,3 +1,42 @@
+import { Skeleton } from "@/components/skeleton";
+
 export default function PostLoading() {
-  return <p>carregando...</p>;
+  return (
+    <div className="flex gap-12 flex-col">
+      <Skeleton className="h-9 w-60 m-auto" />
+
+      <Skeleton className="h-64 w-full" />
+
+      <div className="max-w-[660px] w-full m-auto flex flex-col gap-12">
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-96" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
+        </div>
+      </div>
+    </div>
+  );
 }
